003fde1211c0593202b357e379a37c5ea9196220
bsc-inf-08-20 <bsc-inf-08-20@unima.ac.mw>
2025-02-10 02:02:57 +0200

    connect to the database

---
+++ src/entities/Customer.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity() // Marks this class as a database table
+export class Customer {
+  @PrimaryGeneratedColumn() // Auto-incremented primary key
+  customer_id: number;
+
+  @Column({ length: 100 }) // A string column with a max length of 100
+  name: string;
+
+  @Column({ unique: true }) // A unique email column
+  email: string;
+
+  @Column({ length: 15 }) // A string column for phone numbers
+  phone_number: string;
+
+  @Column('text') // A column for long text (e.g., address)
+  address: string;
+
+  @CreateDateColumn() // Automatically stores when the record is created
+  created_at: Date;
+}
