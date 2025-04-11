456238324129006f83d07cc7c1a47e6f8eaa8eed
Bsc-29-20 <Bsc-29-20@unima.ac.mw>
2025-04-11 08:48:43 +0200

    Changes to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request} from '@nestjs/common';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    @UseGuards(JwtAuthGuard)
+    @Get('me')
+    async getProfile(@Request() req) {
+    return req.user; // Assuming JwtAuthGuard populates req.user
+}
+}
